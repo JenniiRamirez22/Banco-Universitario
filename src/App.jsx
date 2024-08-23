567fdbe67f68f42e2ad76c7770c5d6952ad8abfe
Jennifer <1001.26480892.ucla@gmail.com>
2024-08-23 12:17:01 -0400

    seccion services casi lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Services from './components/Services'
 
 function App(){
   return (
     <div>
       <Navbar />
       <Home />
+      <Services />
     </div>);
 }
 
